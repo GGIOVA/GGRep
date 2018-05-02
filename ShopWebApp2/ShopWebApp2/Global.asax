58eaad64ff3bf859ac00f14212b2c3e4b0eb96a8
LoreWubWub <l.garavaglia@itsrizzoli.it>
2018-05-02 17:47:32 +0200

    WebApp2

---
+++ ShopWebApp2/ShopWebApp2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopWebApp2.MvcApplication" Language="C#" %>
---
+++ ShopWebApp2/ShopWebApp2/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopWebApp2.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Cost { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(string Name, string Description, double Cost, int Quantity)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Cost = Cost;
+            this.Quantity = Quantity;
+        }
+
+        public Product(int Id, string Name, string Description, double Cost, int Quantity)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Description = Description;
+            this.Cost = Cost;
+            this.Quantity = Quantity;
+        }
+
+    }
+}
