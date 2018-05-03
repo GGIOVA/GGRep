05db7172115c8c713d7c881de2fe877875c27144
LoreWubWub <l.garavaglia@itsrizzoli.it>
2018-05-03 17:23:12 +0200

    REST Services

---
+++ WebServices2/Models/Label.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServices2.Models
+{
+    public class Label
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Length { get; set; }
+
+        public Label(string Name, string Description)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            Length = Description.Length;
+        }
+    }
+}
